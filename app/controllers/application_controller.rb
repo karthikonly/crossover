14e638bdf522bb28d7e548479700fd26bd5bcfd9
Karthik L <L.Karthik@Ericsson.com>
2017-03-25 12:41:49 +0530

    devise integration, generate views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  # all users have to be authenticated
+  before_action :authenticate_user!
+
   # cancan exception catching code
   rescue_from CanCan::AccessDenied do |exception|
     respond_to do |format|
